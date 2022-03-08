27395b92b013cdc24d4c245c779ae1771c2e0c53
hrabit64 <hzser123@hanyang.ac.kr>
2022-03-08 23:32:58 +0900

    [FEAT] add profilecontrollerTest

--- src/test/java/com/example/demo/web/ProfileControllerTest.java
+++ src/test/java/com/example/demo/web/ProfileControllerTest.java
@@ -2,11 +2,28 @@ package com.example.demo.web;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
     @Test
     public void real_profile이_조회된다(){
         //given
@@ -41,5 +58,18 @@ public class ProfileControllerTest {
 
         //then
         assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception{
+        //given
+        String expected = "default";
+
+        //when
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/profle",String.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(expected);
     }
 }
